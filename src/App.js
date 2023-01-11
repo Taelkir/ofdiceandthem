3e55bdb26fecb42feb95b4a85a7e54637936f9b2
Tom Stanley <tom.stanley37@gmail.com>
2023-01-11 16:52:45 +0000

    Our story begins

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./ofdiceandthem-500px.png";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Of Dice and Them is a podcast, coming 6th February 2023.</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
